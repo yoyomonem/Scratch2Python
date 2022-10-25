14a952244de321af1ad61d7aba3ec64a87eef48b
Y Nasr <yoyo.monem22@gmail.com>
2022-10-25 16:58:06 +0200

    Initial commit

---
+++ window.py
@@ -0,0 +1,21 @@
+import pygame
+import tkinter as tk
+from version_info import print_version
+import time as time_controller
+
+window = tk.Tk()
+window.title("Scratch 2 Offline Editor written in Python 3.10.8")
+print_version()
+time_controller.sleep(2)
+print("Starting editor . . .")
+time_controller.sleep(6)
+is_window_open = True
+window.mainloop()
+is_window_open = False
+print("Editor exited")
+time_controller.sleep(2)
+print("Thanks for using the Scratch 2 Offline Editor written in Python 3.10.8.\nWe hope you have a nice day! "
+      "😃\nScratch will appreciate this... err... maybe.")
+time_controller.sleep(2)
+print("Aborting . . .")
+time_controller.sleep(6)
