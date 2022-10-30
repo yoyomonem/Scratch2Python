727442b68379e7e802abbd26ad7da365f1d0a886
Y Nasr <yoyo.monem22@gmail.com>
2022-10-30 15:20:56 +0100

    Update 3 files

--- version_info.py
+++ version_info.py
@@ -1,2 +1,2 @@
 def print_version():
-    print(f"\nScratch editor (Version: 2.0, built with Python 3.10.8; needs Python 3.10.8 or later)")
+    print(f"\nScratch editor (Version: 2.0, built with Python 3.10.8)")
