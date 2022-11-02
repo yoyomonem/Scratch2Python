4aea4566ebb7bd222e661c4da6c2e8abac48ed9c
Y Nasr <yoyo.monem22@gmail.com>
2022-11-02 19:41:07 +0100

    Update 2 files

--- window.py
+++ window.py
@@ -6,6 +6,44 @@ from version_info import print_version
 import time as time_controller
 from tkinter import ttk
 
+debug_enabled=False
+
+def drag_start(event):
+     widget = event.widget
+     widget.startX = event.x
+     widget.startY = event.y
+     widget.initX= widget.winfo_x()
+     widget.initY= widget.winfo_y()
+def drag_motion(event):
+      widget = event.widget
+      x = widget.winfo_x() - widget.startX + event.x
+      y = widget.winfo_y() - widget.startY + event.y
+      widget.place(x=x, y=y)
+
+
+def duplicate_block(event):
+      widget = event.widget
+      x = widget.winfo_x() - widget.startX + event.x
+      y = widget.winfo_y() - widget.startY + event.y
+
+
+      if widget.initX > 800 and widget.initX < 1490 and widget.initY > 50 and widget.initY < 790:
+            if debug_enabled: print('Block not duplicated')
+      else:
+            block1 = Label(editor, bg="black", width=3, height=3)
+            block1.place(x=600, y=60)
+            block1.bind("<Button-1>", drag_start)
+            block1.bind("<B1-Motion>", drag_motion)
+            block1.bind("<ButtonRelease-1>", duplicate_block)
+            if debug_enabled: print('Block duplicated')
+
+
+      if x > 800 and x < 1490 and y > 50 and y < 790:
+            donothing = True
+      else:
+            widget.destroy()
+            if debug_enabled: print("Block removed")
+
 
 #json_file = codecs.open(".\\assets\\project_data.json", "r+")
 #with json:
@@ -23,6 +61,13 @@ w.create_rectangle(800, 50, 1490, 790, fill="lightgrey", outline = 'lightgrey')
 w.create_rectangle(500, 50, 790, 790, fill="lightgrey", outline = 'lightgrey')
 w.create_rectangle(10, 50, 490, 500, fill="white", outline = 'grey')
 w.create_rectangle(10, 510, 490, 790, fill="white", outline = 'grey')
+
+block1 = Label(editor, bg="black", width=3, height=3)
+block1.place(x=600,y=60)
+block1.bind("<Button-1>", drag_start)
+block1.bind("<B1-Motion>", drag_motion)
+block1.bind("<ButtonRelease-1>", duplicate_block)
+
 w.pack()
 #ttk.Separator(    master=editor,    orient=VERTICAL,    style='TSeparator',    class_= ttk.Separator,    takefocus= 1,    cursor='man'    ).pack(fill=Y, pady=10, expand=True)
 print_version()
